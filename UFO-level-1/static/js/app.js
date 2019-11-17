7ca58e373103cfa29667e5dfbc831d36161151a7
elzster <hakim.elie1@gmail.com>
2019-11-17 11:46:54 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,10 @@
 // from data.js
 var tableData = data;
 
+//select the tbody in html to render dataset onto.
 var tbody = d3.select("tbody")
 
+//populates entire dataset onto html page.
 data.forEach((ufoSightings) => {
     var row = tbody.append("tr");
     Object.entries(ufoSightings).forEach(([key, value]) => {
@@ -14,15 +16,17 @@ data.forEach((ufoSightings) => {
 //filter button
 var button = d3.select("#filter-btn");
 
-button.on("click", function() {
+//function for refresh stop.
+var handleInput = () => {
+  //Enter key Fix
+  d3.event.preventDefault();
   
   //Define Input Element Variable
   var inputElement = d3.select('#datetime');
 
-
-
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
+  
   // d3.event.prevent.Default()
 
   console.log(inputValue);
@@ -32,4 +36,13 @@ button.on("click", function() {
   var filteredData = tableData.filter(data => data.datetime === inputValue);
   console.log(filteredData)
 
-});  
+};
+
+// button.on("click", handleInput);
+button.on('click', handleInput);
+
+//Enter Key Fix
+d3.select("form").on('submit', handleInput);
+
+//will need to loop through filtered results to display new table.
+
